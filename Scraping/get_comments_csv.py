baa6348eedfcaa3642386b34a43ed78bf514b897
kaneko <bull.31415@gmail.com>
2020-10-15 11:26:23 +0900

    コメントを取得し、csvファイルへ出力する処理　完成

---
+++ Scraping/get_comments_csv.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import csv
+
+client_id = 'zfpe6zkbszk1hez9g9hcxykn14ew4i'
+video_id = '766065120'
+index_count = 0
+# 一回目のリクエスト
+url = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?content_offset_seconds=0'
+headers = {'client-id': client_id}
+r = requests.get(url, headers=headers)
+row_data = r.json()
+    
+with open('extracted_data.csv', 'a') as f:
+    writer = csv.writer(f)
+    for comment in row_data['comments']:
+        writer.writerow([
+            index_count, 
+            comment['content_offset_seconds'],
+            comment['message']['body'],
+            'emoticons' in comment['message']
+        ])
+        index_count += 1
+
+# 二回目以降のリクエスト
+while '_next' in row_data:
+    url = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?cursor=' + row_data['_next']
+    headers = {'client-id': client_id}
+    r = requests.get(url, headers=headers)
+    row_data = r.json()
+
+    with open('extracted_data.csv', 'a') as f:
+        writer = csv.writer(f)
+        for comment in row_data['comments']:
+            writer.writerow([
+                index_count, 
+                comment['content_offset_seconds'],
+                comment['message']['body'],
+                'emoticons' in comment['message']
+            ])
+            index_count += 1
+
+# todo 最後にrow_dataを出力する処理
+# コメント取得完了 or エラー　を判定するため
