1b9a19c84ebe3b1d94fdffc7a87f72fef9079734
kaneko <bull.31415@gmail.com>
2020-11-01 15:43:06 +0900

    templateの作成 home.html, grapth.html

--- app/app.py
+++ app/app.py
@@ -3,8 +3,13 @@ import responder
 api = responder.API()
 
 @api.route("/")
-def hello_world_html(req, resp):
-    resp.html = api.template('index.html')
+def home(req, resp):
+    resp.html = api.template('home.html')
 
+@api.route("/grapht")
+def grapth(req, resp):
+    resp.html = api.template('grapht.html', url=req.params['url'])
+
+    
 if __name__ == '__main__':
     api.run(port=5000, address='0.0.0.0')
