95c3b91f873465e6c9464dfeb8c06eab7159abfc
kaneko <bull.31415@gmail.com>
2020-11-03 17:21:14 +0900

    TwitchVideoクラス get_tokenの処理を記述

--- app/test.py
+++ app/test.py
@@ -2,12 +2,12 @@ import json
 
 import pytest
 
-
 from twitch import TwitchVideo
 
 @pytest.fixture()
 def video():
     return TwitchVideo('https://www.twitch.tv/videos/739949384')
+
 
 class TestTwitch:
     def test_init(self, video):
@@ -19,14 +19,14 @@ class TestTwitch:
         assert video.client_secret == config['twitch']['client_secret']
         assert video.app_access_token == config['twitch']['app_access_token']
 
-    # mockを使ったテスト
-    def test_get_info(self, mocker, video):
-        responseMock = mocker.Mock()
-        responseMock.status_code = 200
-        with open('get_info.json') as f:
-            responseMock.text = f.read()
 
-        mocker.patch('requests.get').return_value = responseMock
+    def test_get_info(self, mocker, video):
+        res_mock = mocker.Mock()
+        res_mock.status_code = 200
+        with open('get_info.json') as f:
+            res_mock.text = f.read()
+
+        mocker.patch('requests.get').return_value = res_mock
 
         video_info = video.get_info()
         assert 'user_name' in video_info
@@ -36,8 +36,9 @@ class TestTwitch:
         assert 'duration_minutes' in video_info
         assert type(video_info['duration_minutes']) is int
 
-    # 実際にtwitch apiを叩くテスト
-    # def test_get_info(self, video):
+
+    # ------実際にtwitch apiを叩くテスト----------
+    # def test_get_info_real_api(self, video):
     #     video_info = video.get_info()
     #     assert 'user_name' in video_info
     #     assert 'title' in video_info
@@ -45,28 +46,51 @@ class TestTwitch:
     #     assert 'url' in video_info
     #     assert 'duration_minutes' in video_info
     #     assert type(video_info['duration_minutes']) is int
+    # -----------------------------------------------------
 
-    # mockを使ったテスト
-    def test_get_info_with_invalid_token(self):
+
+    # app_access_tokenの期限が切れて、無効になっていた場合のテスト
+    def test_get_info_with_invalid_token(self, mocker, video):
+        res_mock = mocker.Mock()
+        res_mock.status_code = 404
+        res_mock.text = '{"error": "Unauthorized", "status": 401, "message": "Invalid OAuth token"}'
+
+        mocker.patch('requests.get').return_value = res_mock
+
+        # todo requestsが投げる例外の正体がわからない
+        with pytest.raises(Exception):
+            video.get_info()
+
+
+    def test_get_token(self, mocker, video):
         with open('config.json', 'r') as f:
             config = json.load(f)
-        app_access_tokne = config['twitch']['app_access_token']
-        config['twitch']['app_access_token'] = '' # tokenがない場合をテストするためからにする
 
-        with open('data/dst/test_new_update.json', 'w') as f:
-            json.dump(d_update, f, indent=2, ensure_ascii=False)
+        get_token_mock = mocker.Mock()
+        get_token_mock.status_code = 200
+        get_token_mock.text = '{"access_token": "sample_token_for_test", "expires_in": 5669710, "token_type": "bearer"}'
 
-        assert video.app_access_token == config['twitch']['app_access_token']
+        mocker.patch('requests.post').return_value = get_token_mock
 
-    # todo 実際にtwitch apiを叩くテスト
-    # def test_get_info_with_invalid_token(self):
+        video._get_token()
+        assert video.app_access_token == 'sample_token_for_test'
+
+        # テスト用に変更したtokenを元に戻す
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent=2, ensure_ascii=False)
+        # テスト用に変更したtokenが元に戻っているか確認
+        # assert video.app_access_token == config['twitch']['app_access_token']
 
 
+    # ------実際にtwitch apiを叩くテスト----------
+    # def test_get_token_real_api(self, mocker, video):
+    #     with open('config.json', 'r') as f:
+    #         config = json.load(f)
 
-
-    # def test_get_token(self, video):
     #     video.get_token()
-    #     assert type(video.app_access_token) is str
+    #     assert video.app_access_token != config['twitch']['app_access_token']   
+    # -------------------------------------------------
+
 
     # def test_get_comments(self, video):
     #     video.get_token()
--- app/twitch.py
+++ app/twitch.py
@@ -14,11 +14,31 @@ class TwitchVideo:
         self.client_secret = config['twitch']['client_secret']
         self.app_access_token = config['twitch']['app_access_token']
 
-    # todo private化する
-    def get_token(self):
-        
-        return self.video_id
 
+    def get_token(self):
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+
+        url = 'https://id.twitch.tv/oauth2/token'\
+              '?client_id=' + self.client_id + \
+              '&client_secret=' + self.client_secret + \
+              '&grant_type=client_credentials'
+
+        res = requests.post(url)
+        print(res.text)
+        res_text_dict = json.loads(res.text)
+
+        config['twitch']['app_access_token'] = res_text_dict['access_token']
+
+        # configのapp_access_tokenを更新する
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent=2, ensure_ascii=False)
+
+        # インスタンス変数を更新する
+        self.app_access_token = res_text_dict['access_token']
+
+
+    #　この関数は例外を投げる（raise_for_status()）
     def get_info(self):
 
         url = 'https://api.twitch.tv/helix/videos?id=' + self.video_id
@@ -27,6 +47,7 @@ class TwitchVideo:
             'Client-Id': self.client_id
         }
         res = requests.get(url, headers=headers)
+        res.raise_for_status()
         res_text_dict = json.loads(res.text)
 
         duration_list = re.split('h|m|s', res_text_dict['data'][0]['duration'])
@@ -42,5 +63,6 @@ class TwitchVideo:
 
         return video_info
     
+
     def get_comments(self):
         return 'dict'
