3ec0cbd3b4be2527cce5705ace4d6ec8da0a66ba
kaneko <bull.31415@gmail.com>
2020-11-15 11:05:55 +0900

    Dockerfileの変更

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:3.7
 WORKDIR /app
 
 COPY app/requirements.txt ./
-
 RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 COPY app/ ./
+
+CMD python run.py
--- app/run.py
+++ app/run.py
@@ -3,4 +3,4 @@ from urls import api
 # debug=Trueをつけてもデバッグモードにならないため
 # uvicorn app:api --debug --port 5000 --host 0.0.0.0で起動する
 if __name__ == '__main__':
-    api.run(port=5000, address='0.0.0.0', debug=True)
+    api.run(port=5000, address='0.0.0.0')
