b02fc2e8484b0614fb6d564f701ec366c5f35b47
kaneko <bull.31415@gmail.com>
2020-11-24 10:35:07 +0900

    youtubeクラスの作成

--- app/external/youtube.py
+++ app/external/youtube.py
@@ -10,8 +10,6 @@ from external.video import Video
 from exception import VideoNotFoundError
 
 
-
-
 class ContinuationURLNotFound(Exception):
     pass
 
@@ -58,6 +56,116 @@ class YoutubeVideo(Video):
         return video_info
 
 
+    def get_comment_data(self):
+        try:
+            comments = self._get_chat_replay_data()
+        except LiveChatReplayDisabled:
+            print(self.video_id + " is disabled Livechat replay")
+            raise(VideoNotFoundError('動画情報が取得できません'))
+        except ContinuationURLNotFound:
+            print(self.video_id + " can not find continuation url")
+            raise(VideoNotFoundError('動画情報が取得できません'))
+        except Exception:
+            print("Unexpected error:" + str(sys.exc_info()[0]))
+            raise(VideoNotFoundError('動画情報が取得できません'))
+
+        duration_list = re.split(':', comments[-1]['time'])
+        # 配信時間が1時間未満の場合 -> 分：秒、1時間以上の場合 -> 時間：分：秒となるため場合分け
+        if len(duration_list) == 3:
+                duration_minutes = int(duration_list[0]) * 60 + int(duration_list[1]) + 1
+        else:
+                duration_minutes = int(duration_list[0]) + 1
+        comment_count = [0] * duration_minutes
+        w_count = [0] * duration_minutes
+
+        for comment in comments:
+            commented_time_list = re.split(':', comment['time'])
+            # コメント時間が1時間未満の場合 -> 分：秒、1時間以上の場合 -> 時間：分：秒となるため場合分け
+            if len(commented_time_list) == 3:
+                commented_minute = int(commented_time_list[0]) * 60 + int(commented_time_list[1])
+            else:
+                # コメント時間がマイナスのコメントは無視
+                if commented_time_list[0][0] == '-':
+                    continue
+                commented_minute = int(commented_time_list[0])
+
+            comment_count[commented_minute] += 1
+            # コメントにwがあれば、w_countを増やす
+            t = comment['text']
+            if (t[-1] == 'w') or (t[-1] == 'W') or (t[-1] == 'ｗ') or (t[-1] == 'W') or (t[-1] == '草'):
+                w_count[commented_minute] += 1
+
+        comments_data = {
+            'comment_count': comment_count,
+            'w_count': w_count
+        }
+        return comments_data
+        
+
+    def _get_chat_replay_data(self):
+        youtube_url = "https://www.youtube.com/watch?v="
+        target_url = youtube_url + self.video_id
+        continuation_prefix = "https://www.youtube.com/live_chat_replay?continuation="
+        result = []
+        session = requests.Session()
+        continuation = self._get_initial_continuation(target_url, session)
+        count = 1
+        while(1):
+            if not continuation:
+                break
+
+            try:
+                ytInitialData = self._get_ytInitialData(continuation_prefix + continuation, session)
+                if not 'actions' in ytInitialData['continuationContents']['liveChatContinuation']:
+                    break
+                for action in ytInitialData['continuationContents']['liveChatContinuation']['actions']:
+                    if not 'addChatItemAction' in action['replayChatItemAction']['actions'][0]:
+                        continue
+                    chatlog = {}
+                    item = action['replayChatItemAction']['actions'][0]['addChatItemAction']['item']
+                    if 'liveChatTextMessageRenderer' in item:
+                        chatlog = self._convert_chatreplay(item['liveChatTextMessageRenderer'])
+                    elif 'liveChatPaidMessageRenderer' in item:
+                        chatlog = self._convert_chatreplay(item['liveChatPaidMessageRenderer'])
+
+                    if 'liveChatTextMessageRenderer' in item or 'liveChatPaidMessageRenderer' in item:
+                        chatlog['video_id'] = self.video_id
+                        chatlog['Chat_No'] = ("%05d" % count)
+                        result.append(chatlog)
+                        count += 1
+
+                continuation = self._get_continuation(ytInitialData)
+
+            except requests.ConnectionError:
+                print("Connection Error")
+                continue
+            except requests.HTTPError:
+                print("HTTPError")
+                break
+            except requests.Timeout:
+                print("Timeout")
+                continue
+            except requests.exceptions.RequestException as e:
+                print(e)
+                break
+            except KeyError as e:
+                print("KeyError")
+                print(e)
+                break
+            except SyntaxError as e:
+                print("SyntaxError")
+                print(e)
+                break
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print("Unexpected error:" + str(sys.exc_info()[0]))
+                print(e)
+                break
+
+        return(result)
+
+
     def _get_ytInitialData(self, target_url, session):
         headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
         html = session.get(target_url, headers=headers)
@@ -70,15 +178,18 @@ class YoutubeVideo(Video):
                     if 'ytInitialData' in line:
                         return(json.loads(line.strip()[len('window["ytInitialData"] = '):-1]))
 
+
     def _get_continuation(self, ytInitialData):
         continuation = ytInitialData['continuationContents']['liveChatContinuation']['continuations'][0].get('liveChatReplayContinuationData', {}).get('continuation')
         return(continuation)
+
 
     def _check_livechat_replay_disable(self, ytInitialData):
         conversationBarRenderer = ytInitialData['contents']['twoColumnWatchNextResults']['conversationBar'].get('conversationBarRenderer', {})
         if conversationBarRenderer:
             if conversationBarRenderer['availabilityMessage']['messageRenderer']['text']['runs'][0]['text'] == 'この動画ではチャットのリプレイを利用できません。':
                 return(True)
+
 
     @retry(ContinuationURLNotFound, tries=3, delay=1)
     def _get_initial_continuation(self, target_url,session):
@@ -141,112 +252,3 @@ class YoutubeVideo(Video):
             chatlog['type'] = 'NORMALCHAT'
 
         return(chatlog)
-
-    def _get_chat_replay_data(self):
-        youtube_url = "https://www.youtube.com/watch?v="
-        target_url = youtube_url + self.video_id
-        continuation_prefix = "https://www.youtube.com/live_chat_replay?continuation="
-        result = []
-        session = requests.Session()
-        continuation = ""
-
-        try:
-            continuation = self._get_initial_continuation(target_url, session)
-        except LiveChatReplayDisabled:
-            print(self.video_id + " is disabled Livechat replay")
-            raise LiveChatReplayDisabled
-        except ContinuationURLNotFound:
-            print(self.video_id + " can not find continuation url")
-            raise ContinuationURLNotFound
-        except Exception:
-            print("Unexpected error:" + str(sys.exc_info()[0]))
-
-        count = 1
-        while(1):
-            if not continuation:
-                break
-
-            try:
-                ytInitialData = self._get_ytInitialData(continuation_prefix + continuation, session)
-                if not 'actions' in ytInitialData['continuationContents']['liveChatContinuation']:
-                    break
-                for action in ytInitialData['continuationContents']['liveChatContinuation']['actions']:
-                    if not 'addChatItemAction' in action['replayChatItemAction']['actions'][0]:
-                        continue
-                    chatlog = {}
-                    item = action['replayChatItemAction']['actions'][0]['addChatItemAction']['item']
-                    if 'liveChatTextMessageRenderer' in item:
-                        chatlog = self._convert_chatreplay(item['liveChatTextMessageRenderer'])
-                    elif 'liveChatPaidMessageRenderer' in item:
-                        chatlog = self._convert_chatreplay(item['liveChatPaidMessageRenderer'])
-
-                    if 'liveChatTextMessageRenderer' in item or 'liveChatPaidMessageRenderer' in item:
-                        chatlog['video_id'] = self.video_id
-                        chatlog['Chat_No'] = ("%05d" % count)
-                        result.append(chatlog)
-                        count += 1
-
-                continuation = self._get_continuation(ytInitialData)
-
-            except requests.ConnectionError:
-                print("Connection Error")
-                continue
-            except requests.HTTPError:
-                print("HTTPError")
-                break
-            except requests.Timeout:
-                print("Timeout")
-                continue
-            except requests.exceptions.RequestException as e:
-                print(e)
-                break
-            except KeyError as e:
-                print("KeyError")
-                print(e)
-                break
-            except SyntaxError as e:
-                print("SyntaxError")
-                print(e)
-                break
-            except KeyboardInterrupt:
-                break
-            except Exception as e:
-                print("Unexpected error:" + str(sys.exc_info()[0]))
-                print(e)
-                break
-
-        return(result)
-
-
-    def get_comment_data(self):
-        comments = self._get_chat_replay_data()
-        duration_list = re.split(':', comments[-1]['time'])
-        # 配信時間が1時間未満の場合 -> 分：秒、1時間以上の場合 -> 時間：分：秒となるため場合分け
-        if len(duration_list) == 3:
-                duration_minutes = int(duration_list[0]) * 60 + int(duration_list[1]) + 1
-        else:
-                duration_minutes = int(duration_list[0]) + 1
-        comment_count = [0] * duration_minutes
-        w_count = [0] * duration_minutes
-
-        for comment in comments:
-            commented_time_list = re.split(':', comment['time'])
-            # コメント時間が1時間未満の場合 -> 分：秒、1時間以上の場合 -> 時間：分：秒となるため場合分け
-            if len(commented_time_list) == 3:
-                commented_minute = int(commented_time_list[0]) * 60 + int(commented_time_list[1])
-            else:
-                if commented_time_list[0][0] == '-':
-                    continue
-                commented_minute = int(commented_time_list[0])
-
-            comment_count[commented_minute] += 1
-            # コメントにwがあれば、w_countを増やす
-            t = comment['text']
-            if (t[-1] == 'w') or (t[-1] == 'W') or (t[-1] == 'ｗ') or (t[-1] == 'W') or (t[-1] == '草'):
-                w_count[commented_minute] += 1
-
-        comments_data = {
-            'comment_count': comment_count,
-            'w_count': w_count
-        }
-        return comments_data
