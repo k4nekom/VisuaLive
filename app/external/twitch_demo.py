21b27ec451b89a373063d81304dce1a2fd39c51a
kaneko <bull.31415@gmail.com>
2020-11-24 10:46:17 +0900

    demo

--- app/external/twitch_demo.py
+++ app/external/twitch_demo.py
@@ -4,12 +4,12 @@ from external.video import Video
 
 class TwitchVideoDemo(Video):
     def get_info(self):
-        with open('demo/info.json', 'r') as f:
+        with open('external/demo/info.json', 'r') as f:
             video_info = json.load(f)
         return video_info
             
     
     def get_comment_data(self):
-        with open('demo/comment_data.json', 'r') as f:
+        with open('external/demo/comment_data.json', 'r') as f:
             comment_data = json.load(f)        
         return comment_data
