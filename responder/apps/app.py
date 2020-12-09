547b2db48c937602eb322c6d7b21694271a070a7
kaneko <bull.31415@gmail.com>
2020-12-10 08:48:28 +0900

    テストのlog出力先をtest.logに変更

--- responder/apps/app.py
+++ responder/apps/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 from logging import getLogger, config
 from pathlib import Path
@@ -16,9 +17,19 @@ def create_app():
 api = create_app()
 
 def set_logging():
-    with open('config/logging.json', 'r') as f:
-        log_conf = json.load(f)
+    if os.environ['ENV'] == 'development':
+        with open('config/logging.json', 'r') as f:
+            log_conf = json.load(f)['development']
+    elif os.environ['ENV'] =='test':
+        with open('config/logging.json', 'r') as f:
+            log_conf = json.load(f)['test']
 
     config.dictConfig(log_conf)
 
-logger = getLogger('app.development')
+def make_logger():
+    if os.environ['ENV'] == 'development':
+        return getLogger('app.development')
+    elif os.environ['ENV'] =='test':
+        return getLogger('app.test')
+
+logger = make_logger()
